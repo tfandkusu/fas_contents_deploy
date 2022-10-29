64b857602aba17c5ab3be19255f4c580f30269c5
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-10-29 19:53:44 +0900

    GitHubリポジトリ一覧を取得

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import os
+
+from github import Github
+
+token = os.environ["FAS_CONTENTS_GITHUB_TOKEN"]
+g = Github(token)
+
+# Then play with your Github objects:
+for repo in g.get_user().get_repos(visibility="public"):
+    print(repo.name)
