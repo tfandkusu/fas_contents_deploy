59ff5cb1f5e5c4f621e1022afa41c4cf4f093be2
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-10-29 21:03:26 +0900

    README.mdを設置する

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import base64
 import os
+import shutil
 
 from github import Github
+from github.GithubException import UnknownObjectException
+
+shutil.rmtree("public", ignore_errors=True)
 
 token = os.environ["FAS_CONTENTS_GITHUB_TOKEN"]
 g = Github(token)
 
-# Then play with your Github objects:
 for repo in g.get_user().get_repos(visibility="public"):
-    print(repo.name)
+    os.makedirs("public/%s" % (repo.name), exist_ok=True)
+    try:
+        file = repo.get_readme()
+        content = base64.b64decode(file.content)
+        with open("public/%s/README.md" % (repo.name), "wb") as f:
+            f.write(content)
+    except UnknownObjectException:
+        pass
--- public/test.md
+++
@@ -1,5 +0,0 @@
-# Readme.md for test
-
-This is test file for readme.md.
-
-![screenshot](https://fas-contents.web.app/test.png)
